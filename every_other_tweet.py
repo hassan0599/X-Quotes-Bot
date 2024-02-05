67c01b87d42a97529e117ebfb3dbf439a22b7f14
hassan0599 <61388284+hassan0599@users.noreply.github.com>
2024-02-05 18:11:28 -0500

    Update every_other_tweet.py

--- every_other_tweet.py
+++ every_other_tweet.py
@@ -4,6 +4,7 @@ import os
 
 x = main.make_token()
 client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
 token_url = "https://api.twitter.com/2/oauth2/token"
 
 t = main.r.get("token")
