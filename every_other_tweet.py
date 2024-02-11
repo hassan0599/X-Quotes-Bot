24869b3e7d1710f32c8db265fe4ef8c6a6292b45
hassan0599 <61388284+hassan0599@users.noreply.github.com>
2024-02-11 08:11:04 -0500

    Update every_other_tweet.py

--- every_other_tweet.py
+++ every_other_tweet.py
@@ -25,6 +25,9 @@ main.r.set("token", j_refreshed_token)
 
 content = main.parse_quote()
 downloader.download(content[1], limit=5,  output_dir='dataset', adult_filter_off=True, force_replace=False, timeout=60, verbose=True)
-payload = {"text": "{}\n- {}".format(content[0], content[1])}
 media = main.post_image(content[1], refreshed_token)
+payload = {
+  "text": "{}\n- {}".format(content[0], content[1]),
+  "media": media
+}
 main.post_tweet(media, payload, refreshed_token)
